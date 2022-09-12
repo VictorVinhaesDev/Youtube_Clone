7b5eee557b85bd28d61f2dad2345279aa4370192
Victor Brunacio <afterisbest@gmail.com>
2022-09-12 01:33:28 -0300

    Upando projeto todo

---
+++ src/components/Videos.jsx
@@ -0,0 +1,20 @@
+import { Stack, Box } from "@mui/material"
+import {ChannelCard, VideoCard} from "./"
+
+const Videos = ({videos, direction}) => {
+
+  if(!videos?.length) return "Loading..."
+
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2} >
+        {videos.map((item, index)=>(
+            <Box key={index}>
+                {item.id.videoId && <VideoCard video={item} />}
+                {item.id.channelId && <ChannelCard channelDetail={item} />}
+            </Box>
+        ))}
+    </Stack>
+  )
+}
+
+export default Videos
